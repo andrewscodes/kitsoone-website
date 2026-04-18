3acdedcf109182a66ea5ecb151a52d174c1b359e
Andres Felipe <andrortiz97@hotmail.com>
2026-04-17 23:09:20 -0500

    change contact input styles

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,12 +3,19 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'kitsoone-contact',
   standalone: true,
-  imports: [RouterModule, CommonModule, FloatLabelModule, InputTextModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FloatLabelModule,
+    InputTextModule,
+    TextareaModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
